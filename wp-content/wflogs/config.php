0b33a98ba0ae4b1b24b63d84684b5e05cc90d380
Pedro Marques <pedromarqueslx@gmail.com>
2021-02-02 11:22:09 +0000

    default version

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"um <ZT8dg- _Q=_cYv3YIcmFYhtg3JkN;9>hvF5Gr?@o%2tFB(MZ9U3S9`[)&`3j";s:7:"version";s:5:"1.0.4";s:13:"attackDataKey";i:3394;s:11:"wafDisabled";b:0;}
